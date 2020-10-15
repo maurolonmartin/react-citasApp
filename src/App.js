6275015f49f2adaddded636d52be6bb89f79068c
Mauricio Londono Martinez <mauricio.londono@globant.com>
2020-10-15 18:19:32 -0500

    Last changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
+
+  // Citas en LocalStorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  // Arreglo de citas
+  const [ citas, guardarCitas ] = useState(citasIniciales);
+
+  // UseEfect para realizar ciertas operaciones cuando el state cambie
+  useEffect( () => {
+    localStorage.setItem( 'citas', JSON.stringify( citas ) );
+  }, [citas] );
+
+  // Funcion para guardar cada cita
+  const crearCita = cita => {
+    guardarCitas([ ...citas, cita ]);
+  }
+
+  //funcion para eliminar cita por id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter( cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  // Mensaje variable del titulo
+  const titulo = citas.length === 0 ? "Agrega una cita" : "Administra tus citas";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario 
+              crearCita={crearCita}
+            />
+          </div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map(cita =>(
+              <Cita 
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
